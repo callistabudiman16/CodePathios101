3f8469b86329d7a49d9f93bae5e7e89bd4f06252
callistabudiman16 <callistabudiman@yahoo.com>
2025-01-21 21:57:34 -0800

    Push code

---
+++ CodePath Prework Natasha BudimanTests/CodePath_Prework_Natasha_BudimanTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodePath_Prework_Natasha_BudimanTests.swift
+//  CodePath Prework Natasha BudimanTests
+//
+//  Created by Callista Budiman on 20/01/25.
+//
+
+import Testing
+@testable import CodePath_Prework_Natasha_Budiman
+
+struct CodePath_Prework_Natasha_BudimanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
